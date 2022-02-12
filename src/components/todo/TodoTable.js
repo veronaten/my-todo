77806aa90b1a64a40a25b99fe96ff97875e164ba
Veronika <verona.binko@gmail.com>
2022-02-12 20:29:20 +0300

    Feat: add in TodoTable state with array with todos, in Todos in console.log pass array with todos

--- src/components/todo/TodoTable.js
+++ src/components/todo/TodoTable.js
@@ -3,11 +3,18 @@ import Todos from "./Todos"
 import InputBar from "./InputBar"
 
 class TodoTable extends React.Component {
+  state = {
+    todos: [
+      {id: 1, content: 'do tasks'},
+      {id: 2, content: 'learn React'}
+    ]
+  }
+
   render() {
     return (
       <div>
         <InputBar />
-        <Todos />
+        <Todos todos={this.state.todos}/>
       </div>
     )
   }
--- src/components/todo/Todos.js
+++ src/components/todo/Todos.js
@@ -1,10 +1,11 @@
 import React from "react";
 
-const Todos = () => {
-
+const Todos = (props) => {
+  const todos = props.todos
+  console.log(todos)
   return (
     <div>
-      
+    
     </div>
  )
 }
