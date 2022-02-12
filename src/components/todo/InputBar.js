9f276dde2405a9227fcb10c961c031a5d95a1ec6
Veronika <verona.binko@gmail.com>
2022-02-12 20:15:00 +0300

    Feat: in InputBar in input add event handler onChange with function handleChange

--- src/components/todo/InputBar.js
+++ src/components/todo/InputBar.js
@@ -1,12 +1,22 @@
 import React from "react";
 
 class InputBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {content: ''}
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({content: e.target.value})
+    console.log(e.target.value)
+  }
 
   render() {
     return (
       <div>
         <p>Enter your Todo:</p>
-        <input />
+        <input value={this.state.content} onChange={this.handleChange}/>
         <button type="button" >Add Todo</button>
       </div>
     )
